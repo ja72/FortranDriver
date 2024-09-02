df55438382f517ddf6bb7c11725865045d407403
John Alexiou <jalexiou@hotmail.com>
2024-09-02 11:39:45 -0400

    Separated native code declarations into two classes

---
+++ FortranDriverCS/HelperFunctions.cs
@@ -0,0 +1,48 @@
+﻿namespace FortranDriver
+{
+    public static class HelperFunctions
+    {
+        public static double[,] BuildArray(int n, int m, Func<int, int, double> init = null)
+        {
+            var A = new double[n, m];
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (init==null)
+                    {
+                        A[i, j] = 0.0;
+                    }
+                    else
+                    {
+                        A[i, j] = init(i, j);
+                    }
+                }
+            }
+
+            return A;
+        }
+
+        public static void ShowArray(this double[,] A, int width = 11, string formatting = "g4")
+        {
+            int rows = A.GetLength(0), cols = A.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                Console.Write("|");
+                for (int j = 0; j < cols; j++)
+                {
+                    string text = A[i, j].ToString(formatting).PadLeft(width);
+                    if (text.Length>width)
+                    {
+                        text = text.Substring(0, width-1) + "…";
+                    }
+                    Console.Write($" {text}");
+                }
+                Console.WriteLine(" |");
+            }
+            Console.WriteLine();
+        }
+
+    }
+}
---
+++ FortranDriverCS/UnmanagedImports.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+
+namespace FortranDriver
+{
+    public static class UnmanagedImports
+    {
+        // Fortran DLL interface.
+        [DllImport("FortranDriverDLL.dll", EntryPoint = "DoWork", CharSet = CharSet.Ansi, CallingConvention = CallingConvention.Cdecl)]
+        public static extern void DoWork(ref int n, [In, Out] double[,] A, [MarshalAs(UnmanagedType.FunctionPtr)] ActionRefInt callBack);
+
+        // Delegate type.
+        [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
+        // Important the int is passed by ref (else we could use built-in Action<T> instead of delegate).
+        public delegate void ActionRefInt(ref int progress, ref int count);
+
+        public static void TestFortran()
+        {
+            Console.WriteLine("Call Fortran code declared with DllImport()");
+            ActionRefInt callbackHandler = new ActionRefInt(OnUpdateProgress);
+            Console.WriteLine();
+            int n = 10;
+            double[,] A = HelperFunctions.BuildArray(n, n, (i, j) => i==j ? i+1 : 0);
+            A.ShowArray(5);
+            
+            DoWork(ref n, A, callbackHandler);
+
+            A.ShowArray(5);
+        }
+        public static void OnUpdateProgress(ref int progress, ref int count)
+        {
+            Console.WriteLine($" Step: \t\t {progress,2} of {count,2}");
+        }
+    }
+}
